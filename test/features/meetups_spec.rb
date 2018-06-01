660da42104074791bafcf7831c765961c014284a
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-06-01 16:17:01 +0200

    Add comments with questions

--- test/features/meetups_spec.rb
+++ test/features/meetups_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Meetups management", :type => :feature do
     fill_in "search_query", :with => "Java"
     click_button "Search"
 
+    # This doesn't seem to pass for me. Might I be doing something wrong?
     expect(page).to have_text("Search Results")
   end
 end
